e03d69345ccd041f2e0f3a66ae28d713e61d9260
fengliulin <menin@163.com>
2021-03-04 09:53:41 +0800

    单例设计模式_D懒汉式_线程安全_同步方法

---
+++ src/cc/chengheng/设计模式/单例设计模式/D懒汉式_线程安全_同步方法.java
@@ -0,0 +1,27 @@
+package cc.chengheng.设计模式.单例设计模式;
+
+public class D懒汉式_线程安全_同步方法 {
+    public static void main(String[] args) {
+        SingletonThreadSafe instance1 = SingletonThreadSafe.getInstance();
+        SingletonThreadSafe instance2 = SingletonThreadSafe.getInstance();
+
+        System.out.println(instance1 == instance2);
+        System.out.println("instance1.hashCode=" + instance1.hashCode());
+        System.out.println("instance2.hashCode=" + instance2.hashCode());
+    }
+}
+
+class SingletonThreadSafe {
+    private static SingletonThreadSafe instance;
+
+    private SingletonThreadSafe() {
+    }
+
+    // 提供一个静态的公有方法，加入了同步处理的代码，解决了一个线程安全问题
+    public static synchronized SingletonThreadSafe getInstance() {
+        if (instance == null) {
+            instance = new SingletonThreadSafe();
+        }
+        return instance;
+    }
+}

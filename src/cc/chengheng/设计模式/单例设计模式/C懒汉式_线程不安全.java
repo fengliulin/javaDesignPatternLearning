402b9f2b37c799253338d9a1be19a4b2989f186b
fengliulin <menin@163.com>
2021-03-04 09:46:58 +0800

    单例设计模式_C懒汉式_线程不安全

---
+++ src/cc/chengheng/设计模式/单例设计模式/C懒汉式_线程不安全.java
@@ -0,0 +1,27 @@
+package cc.chengheng.设计模式.单例设计模式;
+
+public class C懒汉式_线程不安全 {
+    public static void main(String[] args) {
+        SingletonThreadUnsafe instance1 = SingletonThreadUnsafe.getInstance();
+        SingletonThreadUnsafe instance2 = SingletonThreadUnsafe.getInstance();
+
+        System.out.println(instance1 == instance2);
+        System.out.println("instance1.hashCode=" + instance1.hashCode());
+        System.out.println("instance2.hashCode=" + instance2.hashCode());
+    }
+}
+
+class SingletonThreadUnsafe {
+    private static SingletonThreadUnsafe instance;
+
+    private SingletonThreadUnsafe() {
+    }
+
+    // 提供一个静态的公有方法，当使用到该方法时，才去创建instance, 就是 懒汉式
+    public static SingletonThreadUnsafe getInstance() {
+        if (instance == null) {
+            instance = new SingletonThreadUnsafe();
+        }
+        return instance;
+    }
+}
